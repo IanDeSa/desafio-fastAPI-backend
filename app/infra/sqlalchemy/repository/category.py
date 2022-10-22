fbcaac4876c46efa1c6b350aca69545ce8a53062
Ian Santos <contato.iansantos@gmail.com>
2022-10-21 20:23:09 -0400

    feat: criação dos repositórios de serviço, onde ficam centralizados as funções que vão fazer requisições para a models e também onde pode ser feito as regras de negócios

---
+++ app/infra/sqlalchemy/repository/category.py
@@ -0,0 +1,40 @@
+from sqlalchemy.orm import Session
+from infra.sqlalchemy.schemas import schemas
+from infra.sqlalchemy.models import models
+from sqlalchemy import delete
+
+
+class CategoryRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create(self, category: schemas.Category):
+        db_category: schemas.Category = models.Category(
+            id=category.id,
+            name=category.name,
+        )
+        self.db.add(db_category)
+        self.db.commit()
+        self.db.refresh(db_category)
+        return db_category
+
+    def get_all(self):
+        category = self.db.query(models.Category).all()
+        return category
+
+    def get_by_pk(self, id: int):
+        category = self.db.get(models.Category, id)
+        return category
+
+    def update_by_pk(
+        self, old_category: schemas.Category, new_category: schemas.Category
+    ):
+        self.db.query(models.Category).filter(
+            models.Category.name == old_category.name
+        ).update({"name": new_category.name})
+        self.db.commit()
+
+    def destroy(self, category_id: int):
+        stmt = delete(models.Category).where(models.Category.id == category_id)
+        self.db.execute(stmt)
+        self.db.commit()
---
+++ app/infra/sqlalchemy/repository/product.py
@@ -0,0 +1,52 @@
+from sqlalchemy.orm import Session
+from infra.sqlalchemy.schemas import schemas
+from infra.sqlalchemy.models import models
+from sqlalchemy import delete
+
+
+class ProductRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create(self, product: schemas.Product):
+        db_product: schemas.Product = models.Product(
+            id=product.id,
+            name=product.name,
+            category_id=product.category_id,
+            price=product.price,
+            serie=product.serie,
+        )
+        self.db.add(db_product)
+        self.db.commit()
+        self.db.refresh(db_product)
+        return db_product
+
+    def get_all(self):
+        products = self.db.query(models.Product).all()
+        return products
+
+    def get_by_pk(self, id: int):
+        products = self.db.get(models.Product, id)
+        return products
+
+    def update_by_pk(
+        self, old_product: schemas.Product, new_product: schemas.Product
+    ):
+        self.db.query(models.Product).filter(
+            models.Product.name == old_product.name
+        ).update({"name": new_product.name})
+        self.db.query(models.Product).filter(
+            models.Product.category_id == old_product.category_id
+        ).update({"category_id": new_product.category_id})
+        self.db.query(models.Product).filter(
+            models.Product.price == old_product.price
+        ).update({"price": new_product.price})
+        self.db.query(models.Product).filter(
+            models.Product.serie == old_product.serie
+        ).update({"serie": new_product.serie})
+        self.db.commit()
+
+    def destroy(self, product_id: int):
+        stmt = delete(models.Product).where(models.Product.id == product_id)
+        self.db.execute(stmt)
+        self.db.commit()
