e3e64390017b64775ac7d284735b811f711b2028
Ian Santos <contato.iansantos@gmail.com>
2022-10-21 20:18:24 -0400

    feat: criação do arquivo database onde faz a conexão do sqlalchemy com o banco de dados, gera o db e cria o banco de dados via função

---
+++ app/infra/sqlalchemy/config/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = (
+    "mariadb+pymysql://root:password@localhost:3306/desafio"
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+print(engine.table_names())
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def create_db():
+    Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
